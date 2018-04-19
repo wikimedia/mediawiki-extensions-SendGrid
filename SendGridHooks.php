a4f1793c15f9394827d2ba72ae1c572218dbab2e
Jayprakash12345 <0freerunning@gmail.com>
2018-04-19 18:41:35 +0000

    Remove 'UnitTestList' hook
    Bug: T142120
    Change-Id: I4c4f98f169bda22797030195c71144665c0c358f

--- SendGridHooks.php
+++ SendGridHooks.php
@@ -77,30 +77,4 @@ class SendGridHooks {
 		return false;
 	}
 
-	/**
-	 * Handler for UnitTestsList hook.
-	 * @see http://www.mediawiki.org/wiki/Manual:Hooks/UnitTestsList
-	 * @param array &$files Array of unit test files
-	 * @return bool true in all cases
-	 */
-	public static function onUnitTestsList( &$files ) {
-		// @codeCoverageIgnoreStart
-		$directoryIterator = new RecursiveDirectoryIterator( __DIR__ . '/tests/' );
-
-		/**
-		 * @var SplFileInfo $fileInfo
-		 */
-		$ourFiles = [];
-		foreach ( new RecursiveIteratorIterator( $directoryIterator ) as $fileInfo ) {
-			if ( substr( $fileInfo->getFilename(), -8 ) === 'Test.php' ) {
-				$ourFiles[] = $fileInfo->getPathname();
-			}
-		}
-
-		$files = array_merge( $files, $ourFiles );
-
-		return true;
-		// @codeCoverageIgnoreEnd
-	}
-
 }
