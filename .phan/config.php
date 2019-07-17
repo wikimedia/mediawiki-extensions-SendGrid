5664efaccfbd132d4415d0b474db2954e991a07a
Daimona Eaytoy <daimona.wiki@gmail.com>
2019-07-17 14:59:15 +0000

    Make phan pass
    Change-Id: I026f6f626e6f0285723bf2f6a3fd11e4d4bcd9c2

--- .phan/config.php
+++ .phan/config.php
@@ -2,4 +2,7 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
+$cfg['directory_list'][] = 'vendor/sendgrid';
+$cfg['exclude_analysis_directory_list'][] = 'vendor/sendgrid';
+
 return $cfg;
